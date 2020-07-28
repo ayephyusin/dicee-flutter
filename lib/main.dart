6b4fc614a0760a42c553ed72663f354183808f0a
isgm154 <isgm154@ISGM154>
2020-07-28 16:38:31 +0630

    learning dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber= 1;
+  var rightDiceNumber=2;
+  void generateRandomNo(){
+    setState(() {
+      leftDiceNumber= Random().nextInt(6) +1;
+      rightDiceNumber= Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+               generateRandomNo();
+                //print('Left object is pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                  generateRandomNo();
+                //print('Right object is pressed');
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
